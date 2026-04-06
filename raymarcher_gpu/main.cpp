79530719b2a4e70b7754d18809800894d91a995a
Russ Goetz <russgoetz@gmail.com>
2026-04-05 20:25:59 -0400

    Initial Commit

---
+++ raymarcher_gpu/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  raymarcher_gpu
+//
+//  Created by Russ Goetz on 4/5/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
