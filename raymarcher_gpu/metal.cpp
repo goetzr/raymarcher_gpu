bae4c5426357bec0dbd99131ad6fe1337005be1b
Russ Goetz <russgoetz@gmail.com>
2026-04-07 19:07:30 -0400

    Initial addition

---
+++ raymarcher_gpu/metal.cpp
@@ -0,0 +1,10 @@
+//
+//  metal.cpp
+//  raymarcher_gpu
+//
+//  Created by Russ Goetz on 4/6/26.
+//
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
---
+++ raymarcher_gpu/metal_impl.cpp
@@ -0,0 +1,13 @@
+//
+//  metal_impl.cpp
+//  raymarcher_gpu
+//
+//  Created by Russ Goetz on 4/6/26.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
